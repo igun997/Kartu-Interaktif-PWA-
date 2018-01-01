2770130ef0b787bde737b395a8c8c433006167b2
Indra Gunanda <indra.gunanda@gmail.com>
2018-01-01 10:56:28 +0700

    Tambah Deck

--- js/main.js
+++ js/main.js
@@ -46,6 +46,15 @@ $(document).ready(function() {
     });
   }
 
+  function getDeck() {
+    var data =  getData("https://ibio.phpina.net/rest/getdata/");
+    return data;
+  }
+  function getKartu(id) {
+    var data =  getData("https://ibio.phpina.net/rest/generatecard/"+id+"/1");
+    return data;
+  }
+
   function getData(url) {
     var result = null;
     $.ajax({
@@ -59,7 +68,58 @@ $(document).ready(function() {
     });
     return result;
   }
-
+  $("#kartu").on("click",".listItem",function() {
+    var $this = $(this);
+    $.mobile.loading("show", {
+      text: "Loading",
+      textVisible: true,
+      theme: 'a',
+      textonly: false
+    });
+    var get = getKartu($this.data("id"));
+    if(get == null){
+      get = {status:0};
+    }
+    if(get.status == 1){
+      $.mobile.loading("hide");
+      $("#hasilkan").find(".fotoKartu").attr("src",get.data);
+    }else{
+      $.mobile.loading("hide");
+      $("#hasilkan").find(".kontenHasil").html("<h3>Sepertinya Data Yang Kamu Cari Tidak Ada atau Cek Koneksi Internet Kamu</h3>");
+    }
+  });
+  $("#hasilkan").on("click",".saveImages",function() {
+    console.log("Save Images");
+    var data = $("#hasilkan").find(".fotoKartu").attr("src");
+    download(data, "kartu.png", "image/png");
+  });
+  $("#kartuNav").on("click",function() {
+    $.mobile.loading("show", {
+      text: "Loading",
+      textVisible: true,
+      theme: 'a',
+      textonly: false
+    });
+    var deck = getDeck();
+    if(deck == null){
+      deck = [];
+    }
+    console.log("Dek Kartu Init");
+    console.log(deck);
+    if(deck.length > 0){
+      $.mobile.loading("hide");
+      var html = "";
+      for (let i = 0; i < deck.length; i++) {
+        html +='<li><a href="#hasilkan" type="button" data-id="'+deck[0].id_data+'" class="listItem">'+deck[0].nama+'</a></li>';
+      }
+      $("#kartu").find(".deck").html(html);
+      console.log("Refresh List");
+      $('#kartu').find(".deck").listview('refresh');
+    }else{
+      $.mobile.loading("hide");
+      $("#kartu").find(".deckKonten").html('<div class="ui-body ui-body-a ui-corner-all"><h3 align="center">Belum Ada Daftar Kartu</h3></div><a href="#home" class="ui-btn ui-shadow ui-corner-all ui-btn-icon-left ui-icon-back">Kembali</a>');
+    }
+  });
   function getTokoh(id) {
     var data = getData("https://ibio.phpina.net/rest/getdata/" + id);
     console.log(data);
@@ -69,6 +129,9 @@ $(document).ready(function() {
       theme: 'a',
       textonly: false
     });
+    if(data == null){
+      data = {status:0};
+    }
     if (data.status == 1) {
       $.mobile.loading("hide");
       var modal = $("#dialog_onsuccess");
