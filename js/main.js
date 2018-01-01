15c8d8761fe24b907ae771bb21e9cfb60e92b63f
Indra Gunanda <indra.gunanda@gmail.com>
2018-01-02 06:28:17 +0700

    Final Applications

--- js/main.js
+++ js/main.js
@@ -9,10 +9,9 @@ window.addEventListener('beforeinstallprompt', function(e) {
 
     console.log(choiceResult.outcome);
 
-    if(choiceResult.outcome == 'dismissed') {
+    if (choiceResult.outcome == 'dismissed') {
       console.log('User cancelled home screen install');
-    }
-    else {
+    } else {
       console.log('User added to home screen');
     }
   });
@@ -21,37 +20,83 @@ window.addEventListener('beforeinstallprompt', function(e) {
 
 $(document).ready(function() {
   var data = []
-  $("#proses").on("click",function(){
-    $("#cameras").removeAttr("hidden",false);
+  $("#proses").on("click", function() {
+    $("#cameras").removeAttr("hidden", false);
     instan("qrVid");
   });
+
   function instan(id) {
     let scanner = new Instascan.Scanner({
       video: document.getElementById(id)
     });
     scanner.addListener('scan', function(cId) {
       getTokoh(cId);
-      $("#cameras").attr("hidden",true);
+      $("#cameras").attr("hidden", true);
       scanner.stop();
     });
     Instascan.Camera.getCameras().then(function(cameras) {
-      if(cameras.length > 0){
-        scanner.start(cameras[0]);
-      }else{
+      if (cameras.length > 0) {
+        scanner.start(cameras[1]);
+      } else {
         scanner.start(cameras[0]);
       }
     }).catch(function(e) {
-      $("#cameras").attr("hidden",false);
+      $("#cameras").attr("hidden", false);
       console.error(e);
     });
   }
+  $("#snapCamera").on("click", function() {
+    console.log("Snap Camera");
+    $.mobile.loading("show", {
+      text: "Loading",
+      textVisible: true,
+      theme: 'a',
+      textonly: false
+    });
+    Webcam.snap(function(data_uri) {
+      // $("#kontenCam").attr("hidden", false);
+      // $("#camera").find(".fotoSnap").attr("src", data_uri);
+      Webcam.upload(data_uri, 'https://ibio.phpina.net/rest/uploadai', function(code, text) {
+        var dataText = JSON.parse(text);
+        console.log(dataText);
+        if (code == 200) {
+          $.mobile.loading("hide");
+          var html = "";
+          for (let i = 0; i < dataText.data.description.captions.length; i++) {
+            var d = dataText.data.description.captions[i];
+            html +="<center><p><b>"+d.text+"</b></p></center>";
+          }
+          $("#kontenCam").attr("hidden", false);
+          $("#kontenCam").html("<center><h3>Prediksi</h3></center>"+html);
+        } else {
+          $.mobile.loading("hide");
+          $("#kontenCam").attr("hidden", false);
+          $("#kontenCam").html("<center><h3>" + dataText.msg + "</h3></center>");
+        }
+      });
+    });
+  });
+  $("#cameraNav").on("click", function() {
+    Webcam.set({
+      width: 250,
+      height: 200,
+      image_format: 'jpeg',
+      jpeg_quality: 90
+    });
+    Webcam.attach('#camera_shot');
+  });
+  $("#resetCamera").on("click", function() {
+    $("#kontenCam").attr("hidden", true);
+    Webcam.reset();
+  });
 
   function getDeck() {
-    var data =  getData("https://ibio.phpina.net/rest/getdata/");
+    var data = getData("https://ibio.phpina.net/rest/getdata/");
     return data;
   }
+
   function getKartu(id) {
-    var data =  getData("https://ibio.phpina.net/rest/generatecard/"+id+"/1");
+    var data = getData("https://ibio.phpina.net/rest/generatecard/" + id + "/1");
     return data;
   }
 
@@ -68,7 +113,7 @@ $(document).ready(function() {
     });
     return result;
   }
-  $("#kartu").on("click",".listItem",function() {
+  $("#kartu").on("click", ".listItem", function() {
     var $this = $(this);
     $.mobile.loading("show", {
       text: "Loading",
@@ -77,23 +122,25 @@ $(document).ready(function() {
       textonly: false
     });
     var get = getKartu($this.data("id"));
-    if(get == null){
-      get = {status:0};
+    if (get == null) {
+      get = {
+        status: 0
+      };
     }
-    if(get.status == 1){
+    if (get.status == 1) {
       $.mobile.loading("hide");
-      $("#hasilkan").find(".fotoKartu").attr("src",get.data);
-    }else{
+      $("#hasilkan").find(".fotoKartu").attr("src", get.data);
+    } else {
       $.mobile.loading("hide");
       $("#hasilkan").find(".kontenHasil").html("<h3>Sepertinya Data Yang Kamu Cari Tidak Ada atau Cek Koneksi Internet Kamu</h3>");
     }
   });
-  $("#hasilkan").on("click",".saveImages",function() {
+  $("#hasilkan").on("click", ".saveImages", function() {
     console.log("Save Images");
     var data = $("#hasilkan").find(".fotoKartu").attr("src");
     download(data, "kartu.png", "image/png");
   });
-  $("#kartuNav").on("click",function() {
+  $("#kartuNav").on("click", function() {
     $.mobile.loading("show", {
       text: "Loading",
       textVisible: true,
@@ -101,25 +148,26 @@ $(document).ready(function() {
       textonly: false
     });
     var deck = getDeck();
-    if(deck == null){
+    if (deck == null) {
       deck = [];
     }
     console.log("Dek Kartu Init");
     console.log(deck);
-    if(deck.length > 0){
+    if (deck.length > 0) {
       $.mobile.loading("hide");
       var html = "";
       for (let i = 0; i < deck.length; i++) {
-        html +='<li><a href="#hasilkan" type="button" data-id="'+deck[0].id_data+'" class="listItem">'+deck[0].nama+'</a></li>';
+        html += '<li><a href="#hasilkan" type="button" data-id="' + deck[i].id_data + '" class="listItem">' + deck[i].nama + '</a></li>';
       }
       $("#kartu").find(".deck").html(html);
       console.log("Refresh List");
       $('#kartu').find(".deck").listview('refresh');
-    }else{
+    } else {
       $.mobile.loading("hide");
       $("#kartu").find(".deckKonten").html('<div class="ui-body ui-body-a ui-corner-all"><h3 align="center">Belum Ada Daftar Kartu</h3></div><a href="#home" class="ui-btn ui-shadow ui-corner-all ui-btn-icon-left ui-icon-back">Kembali</a>');
     }
   });
+
   function getTokoh(id) {
     var data = getData("https://ibio.phpina.net/rest/getdata/" + id);
     console.log(data);
@@ -129,8 +177,10 @@ $(document).ready(function() {
       theme: 'a',
       textonly: false
     });
-    if(data == null){
-      data = {status:0};
+    if (data == null) {
+      data = {
+        status: 0
+      };
     }
     if (data.status == 1) {
       $.mobile.loading("hide");
